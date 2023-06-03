eab946abe711eece597dfba3e113bfad3f2a8276
kritik <kritikverma2002@gmail.com>
2023-06-03 23:47:18 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
 const os = require("os");
 
 require("dotenv").config();
@@ -25,31 +24,16 @@ app.use(cors(corsOption));
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// Functions
-
-async function getPublicIPAddress() {
-  try {
-    const response = await axios.get("https://api.ipify.org?format=json");
-    const ipAddress = response.data;
-    return ipAddress;
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
-}
-
 app.get("/", (req, res) => {
   res.send("hello");
   //     res.sendFile('build/index.html', { root: __dirname });
 });
 
 app.get("/api/ip", async (req, res) => {
-  const ipData = await getPublicIPAddress();
-
   const hostname = os.hostname();
 
   res.json({
     status: "Success",
-    ipData,
     hostname,
   });
 
