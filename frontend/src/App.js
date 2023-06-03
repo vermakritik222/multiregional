5c93697e57f78ece45ff170f29ce41eb6642fb22
kritik <kritikverma2002@gmail.com>
2023-06-03 23:49:56 +0530

    new build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
-const os = require('os');
-
 
 
 function App() {
-  const hostname = os.hostname();
   const [data, setData] = useState("");
   const fetchData = async () => {
     console.log("api", process.env.REACT_APP_API_URL);
@@ -28,7 +25,7 @@ function App() {
           Status: <span style={{ fontWeight: "bolder" }}>{data?.status}</span>
         </div>
         <div>Backend hostname: {data?.hostname}</div>
-        <div>Frontend hostname: {hostname}</div>
+        {/* <div>Frontend hostname: {hostname}</div> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
